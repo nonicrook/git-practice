f679cd350772314916dead1ba777a30909873656
Noni Crook <ncrook@keriton.com>
2022-12-21 17:12:46 -0500

    created AppUI vue

---
+++ AppUI.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="heading">
+        <h2>Git and Docker Practice</h2>
+    </div>
+</template>
+
+<script></script>
+
+<style>
+    .heading {
+        align-items: center;
+    }
+
+</style>
