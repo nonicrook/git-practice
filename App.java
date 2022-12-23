023a8f84b6c1fdf286cf10141a5b8c6863ec086c
Dan Kelley <dan@keriton.com>
2022-12-23 14:58:02 -0500

    add replace to make cleaner

--- App.java
+++ App.java
@@ -3,7 +3,7 @@
 public class App {
     public static void main (String[] args){
         // TODO: make this print the name set by an environment variable, and use a default if not set
-        System.out.println("Hey whassup Docker! ENV");
+        System.out.println("Hey whassup Docker! <REPLACE ENV>");
         
     }
 }
