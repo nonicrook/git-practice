5f99ac9dabae48f53075cbbe781cf5085527b8ff
Noni Crook <ncrook@keriton.com>
2023-01-02 13:17:07 -0500

    updated sout

--- java/App.java
+++ java/App.java
@@ -4,7 +4,7 @@ package java;
 public class App {
     public static void main (String[] args){
         // TODO: make this print the name set by an environment variable, and use a default if not set
-        System.out.println("Hey whassup Docker! MAC_NAME");
+        System.out.println("MAC_NAME");
         
     }
 }
