2f7e64baf401e42b485d9253dd0cf7f7481316b5
Noni Crook <ncrook@keriton.com>
2023-01-02 14:55:34 -0500

    added default message

--- java/App.java
+++ java/App.java
@@ -3,8 +3,14 @@ package java;
 
 public class App {
     public static void main (String[] args){
-        // TODO: make this print the name set by an environment variable, and use a default if not set
-        System.out.println("MAC_NAME");
+        // DONE: make this print the name set by an environment variable, and use a default if not set
         
+        String mac_name = System.getenv("MAC_NAME");
+        
+        if (mac_name == null) {
+            System.out.println("I am the default!");
+        } else {
+            System.out.println(mac_name);
+        }
     }
 }
