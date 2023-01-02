6e991c0c263b8cd31dea9363428c4bef67e64aca
Noni Crook <ncrook@keriton.com>
2023-01-02 11:20:57 -0500

    saved changed files

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,5 @@
 FROM openjdk
 WORKDIR /git-practice
 COPY . .
-<<<<<<< HEAD
-
-RUN javac App.java
-RUN ls
-CMD ["java", "App"]
-EXPOSE 3000
-=======
 RUN javac App.java
 CMD ["java", "App"]
->>>>>>> merge-me
--- java/App.java
+++ java/App.java
@@ -3,13 +3,8 @@ package java;
 
 public class App {
     public static void main (String[] args){
-<<<<<<< HEAD:java/App.java
-
-        System.out.println("Hey whassup Docker!!");
-=======
         // TODO: make this print the name set by an environment variable, and use a default if not set
         System.out.println("Hey whassup Docker! <REPLACE ENV>");
->>>>>>> merge-me:App.java
         
     }
 }
