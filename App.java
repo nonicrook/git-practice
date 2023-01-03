d37a1368c007541ad90650c4f5eb834190816fbf
Dan Kelley <dan@keriton.com>
2023-01-03 15:30:51 -0500

    soooorrrry

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
-
+package com.noni;
 
 public class App {
     public static void main (String[] args){
         // TODO: make this print the name set by an environment variable, and use a default if not set
-        System.out.println("Hey whassup Docker! <REPLACE ENV>");
+        System.out.println("Docker! <REPLACE ENV>");
+
+        // NOW I Am just being mean and making tough merge changes
         
     }
 }
